90207475173859546f9309e853aee7f71af11ec8
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-05-15 23:22:45 +0530

    ✨feature: User resolvers added

--- models/user-model.js
+++ models/user-model.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     profilePicture: {
-        type: String,
-        required: true,
-        default: ""
+      type: String,
+      required: true,
+      default: "",
     },
     gender: {
-        type: String,
-        enum: ["male", "female"]
-    }
-}, { timestamps: true })
+      type: String,
+      enum: ["male", "female"],
+    },
+  },
+  { timestamps: true }
+);
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
