0fe0c793cd22caa5086a9373233acff7df3e25ca
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-05-16 10:51:02 +0530

    🐞 fix: Import fixes in merger resolver

--- typeDefs/transaction-typeDef.js
+++ typeDefs/transaction-typeDef.js
@@ -1,51 +1,49 @@
 const transactionTypeDef = `#graphql
-    type Transaction{
-        _id:ID!
-        userId:ID!
-        description:String!
-        paymentType:String!
-        category:String!
-        amount:Float!
-        location:String
-        date:String!
+    type Transaction {
+        _id: ID!
+        userId: ID!
+        description: String!
+        paymentType: String!
+        category: String!
+        amount: Float!
+        location: String
+        date: String!
+        user: User!
     }
     
     type Query {
-        transactions:[Transaction!]
-        transaction(transaction:ID!):Transaction
+        transactions: [Transaction!]
+        transaction(transactionId: ID!): Transaction
     }
 
     type Mutation {
-        createTransaction(input:CreateTransaction!):Transaction!
-        updateTransaction(input:UpdateTransaction!):Transaction!
-        deleteTransaction(transaction:ID!):Transaction!
+        createTransaction(input: CreateTransactionInput!): Transaction!
+        updateTransaction(input: UpdateTransactionInput!): Transaction!
+        deleteTransaction(transactionId: ID!): Transaction!
     }
 
-    input CreateTransaction{
-        description:String!
-        paymentType:String!
-        category:String!
-        amount:Float!
-        location:String
-        date:String!
+    input CreateTransactionInput {
+        description: String!
+        paymentType: String!
+        category: String!
+        amount: Float!
+        location: String
+        date: String!
     }
 
-    input UpdateTransaction {
-        transactionId:ID!
-        description:String
-        paymentType:String
-        category:String
-        amount:Float
-        location:String
-        date:String
+    input UpdateTransactionInput {
+        transactionId: ID!
+        description: String
+        paymentType: String
+        category: String
+        amount: Float
+        location: String
+        date: String
     }
 
     type deleteTransaction {
-        message:String!
+        message: String!
     }
-
-
 `;
 
-
-export default transactionTypeDef
+export default transactionTypeDef;
